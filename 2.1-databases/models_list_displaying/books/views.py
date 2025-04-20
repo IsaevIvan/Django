63c5927f19f4c9f6741905c8cf69e3434e957e0e
IvanIsaev <79138903153@yandex.ru>
2025-04-21 00:17:38 +0700

    2

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from .models import Book
 
 def books_view(request):
-    template = 'books/books_list.html'
+    template = 'books/books_main.html'
     context = {}
     return render(request, template, context)
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/books_list.html', {'books': books})
+    # template = 'books/books_list.html'
+    # context = {}
+    # return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from books import views
 from books.views import books_view
 
 urlpatterns = [
     path('', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books/', views.book_list, name='book_list'),
 ]
